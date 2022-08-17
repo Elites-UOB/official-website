5ac11469477b9ad05b93af14f7c1279f13ef92a9
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-08-18 00:06:57 +0300

    add

---
+++ .nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,41 @@
+export default {
+  "@vite/client": {
+    "isEntry": true,
+    "file": "@vite/client",
+    "css": [
+      "node_modules/@nuxt/content/dist/runtime/components/Prose/ProseCode.vue?vue&type=style&index=0&lang.css",
+      "__uno.css",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue?vue&type=style&index=0&scoped=bc8852ab&lang.css",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-dev.vue?vue&type=style&index=0&scoped=6b2d7187&lang.css",
+      "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue?vue&type=style&index=0&scoped=f37408fc&lang.css"
+    ],
+    "module": true,
+    "resourceType": "script"
+  },
+  "node_modules/@nuxt/content/dist/runtime/components/Prose/ProseCode.vue?vue&type=style&index=0&lang.css": {
+    "file": "node_modules/@nuxt/content/dist/runtime/components/Prose/ProseCode.vue?vue&type=style&index=0&lang.css",
+    "resourceType": "style"
+  },
+  "__uno.css": {
+    "file": "__uno.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue?vue&type=style&index=0&scoped=bc8852ab&lang.css": {
+    "file": "node_modules/@nuxt/ui-templates/dist/templates/error-404.vue?vue&type=style&index=0&scoped=bc8852ab&lang.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-dev.vue?vue&type=style&index=0&scoped=6b2d7187&lang.css": {
+    "file": "node_modules/@nuxt/ui-templates/dist/templates/error-dev.vue?vue&type=style&index=0&scoped=6b2d7187&lang.css",
+    "resourceType": "style"
+  },
+  "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue?vue&type=style&index=0&scoped=f37408fc&lang.css": {
+    "file": "node_modules/@nuxt/ui-templates/dist/templates/error-500.vue?vue&type=style&index=0&scoped=f37408fc&lang.css",
+    "resourceType": "style"
+  },
+  "D:/GitHub/CSIT-Elites-Res/node_modules/nuxt/dist/app/entry": {
+    "isEntry": true,
+    "file": "D:/GitHub/CSIT-Elites-Res/node_modules/nuxt/dist/app/entry",
+    "module": true,
+    "resourceType": "script"
+  }
+}
