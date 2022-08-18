0ee6cdc6ba1700fc7761588d7b1209fb8bebd9cf
zal3 <zainabamjedlatef31@gmail.com>
2022-08-18 17:51:28 +0300

    a

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="bg-gray-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded  ">
+    <nav class="bg-gray-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded w-full ">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
             <a class="flex items-center">
                 <img src="~/assets/CoreTeamLogo.png" class="mr-2 h-9 sm:h-12" alt="Flowbite Logo">
--- components/content/Card.vue
+++ components/content/Card.vue
@@ -1,11 +1,11 @@
 <template>
-    <div class="bg-slate-300 p-4 rounded w-88 flex w-full h-full grid grid-rows-3">
+    <div class="grid grid-cols-3  grid-flow-row bg-slate-300 p-4 rounded  w-full h-full ">
         <div v-for="index in 10" :key="index" class="m-9  cursor-pointer relative p-5 group bg-white border-t-[12px]
             rounded-lg transition-all duration-200 delay-100 ease-in-out hover:shadow-xl hover:shadow-secondary-100 ">
             <div class=" flex flex-col ">
-                <div>
+                <div class=" flex  justify-center ">
                     <img src="https://img.freepik.com/premium-vector/programing-concept-program-code-white-isolated-background_197170-769.jpg?w=2000"
-                        alt="img" class="w-77 h-44 rounded-lg">
+                        alt="img" class=" h-44 rounded-lg w-fit  ">
                 </div>
                 <div class="text-xl font-bold flex justify-center text-gray-600 group">
                     <slot name="title" />
