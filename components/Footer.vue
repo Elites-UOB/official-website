c422922935a333d8438914a38203231923740832
zal3 <zainabamjedlatef31@gmail.com>
2022-08-18 23:15:55 +0300

    a

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,41 +1,33 @@
 <template>
-    <footer class="p-4 bg-gray-500 md:px-6 md:py-8 ">
-        <div class="sm:flex sm:items-center sm:justify-between">
-            
-            <a class="flex items-center mb-4 sm:mb-0">
-                <img src="~/assets/CoreTeamLogo.png" class="mr-3 h-8" alt="Flowbite Logo">
-                <span class="self-center text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-pink-600 ">CSIT Elite</span>
-            </a>
-            <ul class="flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 ">
-                <li>
-                    <NuxtLink to="/about"
-                            class="mr-4 hover:underline md:mr-6">
-                            About
-                        </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/PrivacyPolicy"
-                            class="mr-4 hover:underline md:mr-6">
-                            Privacy Policy
-                        </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/licensing"
-                            class="mr-4 hover:underline md:mr-6">
-                            Licensing
-                        </NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/contact"
-                            class="mr-4 hover:underline md:mr-6">
-                            Contact
-                        </NuxtLink>
-                </li>
-            </ul>
+    <footer class="p-4 bg-gray-300 md:px-6 md:py-8 ">
+        <div class="sm:flex sm:items-center sm:justify-between   ">
+            <div>
+                <a class="flex items-center mb-4 sm:mb-0">
+                    <img src="~/assets/CoreTeamLogo.png" class="mr-3 h-8" alt="Flowbite Logo">
+                    <span
+                        class="self-center text-2xl font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-700 to-pink-600 ">CSIT
+                        Elite</span>
+                </a>
+            </div>
+            <div class="text-black text-xl " >
+                <h1 class=" text-bold "> Join </h1>
+                <div>
+                    <i class="fa-brands fa-telegram"></i>
+                    <a href="https://telegram.me/tarmanau">Join telegram </a>
+                </div>
+                <div>
+                    <i class="fa-solid fa-circle-envelope"></i>
+                    <a href="mailto:mail@example.com"> send email</a>
+                </div>
+
+
+            </div>
+            <div>
+                <h1>Blog Posts</h1>
+            </div>
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto">
         <span class="block text-sm text-gray-500 sm:text-center">© 2022 CSIT . All Rights Reserved.
         </span>
     </footer>
-
 </template>
