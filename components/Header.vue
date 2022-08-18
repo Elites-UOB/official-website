4ba81c3e8e61da1962fefa15b3fcbbf3beb8b771
zal3 <zainabamjedlatef31@gmail.com>
2022-08-18 17:23:27 +0300

    f

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,5 @@
 <template>
-
-    <nav class="bg-gray-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded ">
+    <nav class="bg-gray-50 border-gray-200 px-2 sm:px-4 py-2.5 rounded  ">
         <div class="container flex flex-wrap justify-between items-center mx-auto">
             <a class="flex items-center">
                 <img src="~/assets/CoreTeamLogo.png" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
