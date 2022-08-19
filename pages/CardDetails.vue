46103645cacd8c937afd33086783f36385b6e9fc
fatmanawfal <fatma.nawfal.abdalrazak@gmail.com>
2022-08-19 14:16:49 +0300

    add

--- pages/CardDetails.vue
+++ pages/CardDetails.vue
@@ -2,18 +2,18 @@
   <div>
     <div class="pb-16">
       <div class="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
-        <div class="mb-12 space-y-2 text-center">
+        <!-- <div class="mb-12 space-y-2 text-center">
           <h2 class="text-2xl text-cyan-900 font-bold md:text-4xl">
             Blog Details
           </h2>
-        </div>
+        </div> -->
 
         <div>
           <div
-            class="p-1 rounded-xl group sm:flex space-x-6 bg-gray-100 bg-opacity-50 shadow-md hover:rounded-2xl"
+            class="p-1 mt-10 rounded-xl group sm:flex space-x-6 bg-gray-200 bg-opacity-50 shadow-md hover:rounded-2xl"
           >
             <img
-              src="https://tailus.io/sources/blocks/twocards/preview/images/woman.jpg"
+              src="https://img.freepik.com/premium-vector/programing-concept-program-code-white-isolated-background_197170-769.jpg?w=2000"
               alt="art cover"
               loading="lazy"
               class="h-56 h-full w-1/2 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl"
