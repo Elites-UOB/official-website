813360289d68f4ea0353289070eb3d27ae54da8f
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-20 16:13:02 +0300

    qwd

--- pages/index.vue
+++ pages/index.vue
@@ -1,43 +1,47 @@
 <template>
 
-    <div class="grid grid-cols-3 w-full gap-4 bg-black">
-        <div class="">
-            <span self="center" mb="4" text="xl mid" font="bold" whitespace="nowrap">لمجتمعٍ منتج تقنيًا </span>
-            <div class="text-3xl font-bold text-light mt-6">تعلّم، شارك، وطوّر
+    <div flex="~" justify="between" w="full">
+        <div w="1/4">
+            <span text="xl mid" whitespace="nowrap">لمجتمعٍ منتج تقنيًا </span>
+            <div text="3xl light" m="t-10px" font="bold">تعلّم، شارك، وطوّر
             </div>
-            <div class="text-xl text-mid mt-5">
-                ضمن مشروع قادة التحول الرقمي. الفريق تابع
-                رسميًاإلى كلية علوم الحاسوب وتكنولوجيا
+            <div text="mid" mt="12px" w="298px">
+                ضمن <span text="light">مشروع قادة التحول الرقمي</span>. الفريق تابع
+                رسميًا إلى كلية علوم الحاسوب وتكنولوجيا
                 المعلومات، جامعة البصرة.
             </div>
         </div>
         <!-- ... -->
-        <div class="">
-            <svg width="296" height="449" viewBox="0 0 696 849" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div w="2/4" flex="~" justify="center">
+            <svg m="t-1.25rem" width="166" height="202.33" viewBox="0 0 696 849" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd"
                     d="M344.92 142.769L0 0V92.3893V142.769V285.538V428.308V560.495V702.159V703.265L344.92 848.324L696 703.265V560.495L344.92 703.265L123.186 611.484V479.297L344.92 571.077L696 428.308V285.538L344.92 428.308L123.186 336.527V193.758L344.92 285.538L696 142.769V0L344.92 142.769Z"
                     fill="white" />
             </svg>
-
-
         </div>
         <!-- ... -->
-        <div class="w-full bg-dark ">
-            <div class="p-4 bg-dark rounded-lg md:p-8 ">
-                <dl class="grid grid-cols-2 gap-8 p-4 mx-auto max-w-screen-xl sm:grid-cols-3 xl:grid-cols-6 ">
-                    <div class="flex flex-col justify-center items-center ">
-                        <dt class="mb-2 text-3xl font-extrabold">+34</dt>
-                        <dd class="font-bold text-mid mb-4">مشروعًا </dd>
-                        <dt class="mb-2 text-3xl font-extrabold">+34</dt>
-                        <dd class="font-bold text-mid">متدربًا</dd>
+        <div w="1/4">
+            <div flex="~ col gap-1rem" justify="start" font="bold" w="230px">
+                <div flex="~" justify="between">
+                    <div flex="~ col">
+                        <span text="light 32px">+34</span>
+                        <span text="24px">مشروعاً</span>
                     </div>
-                    <div class="flex flex-col justify-center items-center ml-40">
-                        <dt class="mb-2 text-3xl font-extrabold">+34</dt>
-                        <dd class="font-bold text-mid mb-4">حدثًا</dd>
-                        <dt class="mb-2 text-3xl font-extrabold">+34</dt>
-                        <dd class="font-bold text-mid">صفًا</dd>
+                    <div flex="~ col">
+                        <span text="light 32px">+34</span>
+                        <span text="24px">متدرباً</span>
                     </div>
-                </dl>
+                </div>
+                <div flex="~" justify="between">
+                    <div flex="~ col">
+                        <span text="light 32px">+34</span>
+                        <span text="24px">حدثاً</span>
+                    </div>
+                    <div flex="~ col">
+                        <span text="light 32px">+34</span>
+                        <span text="24px">صفًا</span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
