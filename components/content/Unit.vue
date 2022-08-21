e371480cd788364a6f634869c28fe239dbb91e9f
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-08-21 19:37:25 +0300

    qwd

--- components/content/Unit.vue
+++ components/content/Unit.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div cursor="pointer" @click="$emit('select', unit)" un-text="light" :class="color" border="3px mid" flex="~" items="center" justify="center" w="10rem" h="5rem">
+        <div cursor="pointer" @click="$emit('select', unit)" un-text="light" border="3px mid" flex="~" items="center" justify="center" w="10rem" h="5rem">
             <span>{{ unit.name }}</span>
         </div>
     </div>
@@ -10,6 +10,4 @@
 <script setup>
 const props = defineProps(['unit', 'selected'])
 
-const { getUnitColor } = useUnitColor()
-const color = getUnitColor(props.unit?.name)
 </script>
