139858dfbc74e0a6b80f83dc66d9fcead256bb36
Manar Naseer <79095180+6vnar@users.noreply.github.com>
2022-08-23 01:09:46 +0300

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -50,10 +50,33 @@
                 </div>
             </div>
         </div>
+        <div class="prose prose-gray dark:prose-invert max-w-none" >
+        <div grid="~ cols-4 gap-19.75" sm="gap-13px grid-cols-2 m-6 justify-center " md="gap-20 " lg="grid-cols-4 gap-19">
+            <div  v-if="pending" h="48px" w="48px" text="light" class="i-line-md-loading-twotone-loop" ></div>
+            <MiniEvent v-else :path="event._path" v-for="event in events" :key="event"  >
+                <template #title>{{ event.title }}</template>
+                <template #description>{{ event.description}}</template>
+                <template #time>{{ event.time }}</template>
+                <template #state>
+                    <span :class="textColor(event.state)">{{ stateText(event.state) }}</span>
+                </template>
+                <template #image>
+                    <img :src="event.image" alt="Event image" />
+                </template>
+            </MiniEvent>
+        </div>
+    </div>
         
-        
-
 </template>
-<script setup>
-</script>
 
+
+<script setup>
+const { data: events, pending } = await useAsyncData('events', () => queryContent('events').where({ _type: "markdown" }).limit(4).find())
+const { textColor, states, stateText } = useColorState()
+states.value = [
+    { number: 1, text: "مفتوح" },
+    { number: 2, text: "سينتهي التسجيل" },
+    { number: 3, text: "أنتهى" },
+]
+
+</script>
