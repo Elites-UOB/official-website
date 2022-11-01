9fcae30f74f9e8090ffb302068be7bea726bbc3e
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-11-02 02:24:44 +0300

    app

--- app.vue
+++ app.vue
@@ -5,12 +5,39 @@
 </template>
 
 <script setup lang="ts">
-  useHead({
-    titleTemplate: (titleChunk) => {
-      return titleChunk ? `${titleChunk} - نادي النخبة المطورين` : 'نادي النخبة المطورين';
+const route = useRoute()
+
+useHead({
+  titleTemplate: (titleChunk) => {
+    return titleChunk ? `${titleChunk} - نادي النخبة المطورين` : 'نادي النخبة المطورين';
+  },
+  htmlAttrs: {
+    lang: 'ar',
+    dir: 'rtl'
+  },
+  bodyAttrs: {
+    class: 'bg-light dark:bg-dark m-0 p-0 font-tajawal text-mid',
+  },
+  meta: [
+    { name: 'og:title', content: `${route.meta.title} - نادي النخبة المطورين` },
+    {
+      name: 'og:description', content: `نادي طلابي لتطوير المهارات التقنية وتطوير الحلول التكنولوجية للمجتمع
+المحلي، الفريق ضمن مشروع قادة التحوّل الرقمي التابع رسميًا إلى جامعة
+البصرة، كلية علوم الحاسوب وتكنولوجيا المعلومات.`
     },
-    bodyAttrs: {
-      class: 'bg-light dark:bg-dark m-0 p-0 font-tajawal text-mid',
-    }
-  })
+    { name: 'og:image', content: "/cover.jpg" },
+    { name: 'og:url', content: route.fullPath },
+    { name: 'twitter:title', content: `${route.meta.title} - نادي النخبة المطورين` },
+    {
+      name: 'twitter:description', content: `نادي طلابي لتطوير المهارات التقنية وتطوير الحلول التكنولوجية للمجتمع
+المحلي، الفريق ضمن مشروع قادة التحوّل الرقمي التابع رسميًا إلى جامعة
+البصرة، كلية علوم الحاسوب وتكنولوجيا المعلومات.`
+    },
+    { name: 'twitter:image', content: "/cover.jpg" },
+    { name: 'twitter:card', content: 'summary_large_image' },
+
+  ]
+
+
+})
 </script>
--- components/AppLayout.vue
+++ components/AppLayout.vue
@@ -1,8 +1,5 @@
 <template>
   <div class="sm:pt-6 sm:pb-10">
-    <Html lang="en" dir="rtl"/>
-    <Meta property="og:image" content="/cover.jpg" />
-    <Meta name="twitter:card" content="summary_large_image" />
     <AppLoadingBar />
     <AppNavbar />
     <slot />
