9606940ec79472a2056de0a195307d437c2f49c2
Mosa Nameer <mosa.mn777@gmail.com>
2022-11-19 20:44:39 +0300

    add

--- pages/about.vue
+++ pages/about.vue
@@ -1,90 +1,144 @@
 <template>
-    <div class="max-w-3xl grid grid-cols-2 gap-y-20 grid grid-col-3 justify-items-center py-10 m-auto sm:px-8 text-dark dark:text-light">
+    <div class="max-w-4xl grid grid-cols-1 gap-8 justify-items-center  py-10 m-auto sm:px-8 text-dark dark:text-light">
         <div
-            class="w-60 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D]  border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
-            <img class="w-full"
-                src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
-                alt="Sunset in the mountains">
-            <div class="px-4">
-                <div class="font-bold text-lg">صندوق الكرم الخيري</div>
-                <p class="text-gray-700 text-xs">
-
-                    هو مبادرة ونشاط طلابي ينطلق من طلبة كلية علوم الحاسوب وتكنولوجيا المعلومات في جامعة البصرة. الهدف
-                    منه رعاية الأعمال الخيريّة داخل الكليّة وتكون الأولويّة في صرف الصندوق للطلبة ذوي الدخل دون المحدود.
-                    سمي بأسم صندوق الكرم نسبة إلى الأستاذ الراحل عبد الكريم حسين.
-
-                </p>
+            class="grid grid-cols-3 w-125 h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
+            <div class="col-span-1 overflow-hidden ">
+                <img src="https://media.istockphoto.com/id/537373196/photo/trees-forming-a-heart.jpg?s=612x612&w=0&k=20&c=onZKNjkycICe4q2ZDnKi39z42Ax9tpZT7pph-2e5Seo=" alt="">
             </div>
-            <div class="px-6 pb-2">
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Laravel</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Livewire</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">TailwindCss</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Alpine
-                    js</span>
+            <div class="col-span-2">
+                <div class="px-2">
+                    <div class="font-bold text-lg mt-1">صندوق الكرم الخيري</div>
+                    <p class="text-gray-700 text-xs">
+                        الهدف
+                        منه رعاية الأعمال الخيريّة داخل الكليّة وتكون الأولويّة في صرف الصندوق للطلبة ذوي الدخل دون
+                        المحدود.
+                    </p>
+                </div>
+                <div class="px-4 mt-2">
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Laravel</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Livewire</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">TailwindCss</span>
+                        <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Alpine js</span>
+                </div>
             </div>
         </div>
 
 
         <div
-            class="w-60 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D]  border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
-            <img class="w-full"
-                src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
-                alt="Sunset in the mountains">
-            <div class="px-4">
-                <div class="font-bold text-lg">صندوق الكرم الخيري</div>
-                <p class="text-gray-700 text-xs">
-
-                    هو مبادرة ونشاط طلابي ينطلق من طلبة كلية علوم الحاسوب وتكنولوجيا المعلومات في جامعة البصرة. الهدف
-                    منه رعاية الأعمال الخيريّة داخل الكليّة وتكون الأولويّة في صرف الصندوق للطلبة ذوي الدخل دون المحدود.
-                    سمي بأسم صندوق الكرم نسبة إلى الأستاذ الراحل عبد الكريم حسين.
-
-                </p>
+            class="grid grid-cols-3 w-125 h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
+            <div class="col-span-1 overflow-hidden ">
+                <img src="https://media.istockphoto.com/id/537373196/photo/trees-forming-a-heart.jpg?s=612x612&w=0&k=20&c=onZKNjkycICe4q2ZDnKi39z42Ax9tpZT7pph-2e5Seo=" alt="">
             </div>
-            <div class="px-6 pb-2">
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Laravel</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Livewire</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">TailwindCss</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Alpine
-                    js</span>
+            <div class="col-span-2">
+                <div class="px-2">
+                    <div class="font-bold text-lg mt-1">صندوق الكرم الخيري</div>
+                    <p class="text-gray-700 text-xs">
+                        الهدف
+                        منه رعاية الأعمال الخيريّة داخل الكليّة وتكون الأولويّة في صرف الصندوق للطلبة ذوي الدخل دون
+                        المحدود.
+                    </p>
+                </div>
+                <div class="px-4 mt-2">
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Laravel</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Livewire</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">TailwindCss</span>
+                        <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Alpine js</span>
+                </div>
             </div>
         </div>
-
 
 
         <div
-            class="w-60 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D]  border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
-            <img class="w-full"
-                src="https://media.istockphoto.com/id/1322277517/photo/wild-grass-in-the-mountains-at-sunset.jpg?s=612x612&w=0&k=20&c=6mItwwFFGqKNKEAzv0mv6TaxhLN3zSE43bWmFN--J5w="
-                alt="Sunset in the mountains">
-            <div class="px-4">
-                <div class="font-bold text-lg">صندوق الكرم الخيري</div>
-                <p class="text-gray-700 text-xs">
-
-                    هو مبادرة ونشاط طلابي ينطلق من طلبة كلية علوم الحاسوب وتكنولوجيا المعلومات في جامعة البصرة. الهدف
-                    منه رعاية الأعمال الخيريّة داخل الكليّة وتكون الأولويّة في صرف الصندوق للطلبة ذوي الدخل دون المحدود.
-                    سمي بأسم صندوق الكرم نسبة إلى الأستاذ الراحل عبد الكريم حسين.
-
-                </p>
+            class="grid grid-cols-3 w-125 h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
+            <div class="col-span-1 overflow-hidden ">
+                <img src="https://media.istockphoto.com/id/537373196/photo/trees-forming-a-heart.jpg?s=612x612&w=0&k=20&c=onZKNjkycICe4q2ZDnKi39z42Ax9tpZT7pph-2e5Seo=" alt="">
             </div>
-            <div class="px-6 pb-2">
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Laravel</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Livewire</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">TailwindCss</span>
-                <span
-                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-2">Alpine
-                    js</span>
+            <div class="col-span-2">
+                <div class="px-2">
+                    <div class="font-bold text-lg mt-1">صندوق الكرم الخيري</div>
+                    <p class="text-gray-700 text-xs">
+                        الهدف
+                        منه رعاية الأعمال الخيريّة داخل الكليّة وتكون الأولويّة في صرف الصندوق للطلبة ذوي الدخل دون
+                        المحدود.
+                    </p>
+                </div>
+                <div class="px-4 mt-2">
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Laravel</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Livewire</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">TailwindCss</span>
+                        <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Alpine js</span>
+                </div>
             </div>
         </div>
+
+
+        <div
+            class="grid grid-cols-3 w-125 h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
+            <div class="col-span-1 overflow-hidden ">
+                <img src="https://media.istockphoto.com/id/537373196/photo/trees-forming-a-heart.jpg?s=612x612&w=0&k=20&c=onZKNjkycICe4q2ZDnKi39z42Ax9tpZT7pph-2e5Seo=" alt="">
+            </div>
+            <div class="col-span-2">
+                <div class="px-2">
+                    <div class="font-bold text-lg mt-1">صندوق الكرم الخيري</div>
+                    <p class="text-gray-700 text-xs">
+                        الهدف
+                        منه رعاية الأعمال الخيريّة داخل الكليّة وتكون الأولويّة في صرف الصندوق للطلبة ذوي الدخل دون
+                        المحدود.
+                    </p>
+                </div>
+                <div class="px-4 mt-2">
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Laravel</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Livewire</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">TailwindCss</span>
+                        <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Alpine js</span>
+                </div>
+            </div>
+        </div>
+
+
+        <div
+            class="grid grid-cols-3 w-125 h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
+            <div class="col-span-1 overflow-hidden ">
+                <img src="https://media.istockphoto.com/id/537373196/photo/trees-forming-a-heart.jpg?s=612x612&w=0&k=20&c=onZKNjkycICe4q2ZDnKi39z42Ax9tpZT7pph-2e5Seo=" alt="">
+            </div>
+            <div class="col-span-2">
+                <div class="px-2">
+                    <div class="font-bold text-lg mt-1">صندوق الكرم الخيري</div>
+                    <p class="text-gray-700 text-xs">
+                        الهدف
+                        منه رعاية الأعمال الخيريّة داخل الكليّة وتكون الأولويّة في صرف الصندوق للطلبة ذوي الدخل دون
+                        المحدود.
+                    </p>
+                </div>
+                <div class="px-4 mt-2">
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Laravel</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Livewire</span>
+                    <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">TailwindCss</span>
+                        <span
+                        class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">Alpine js</span>
+                </div>
+            </div>
+        </div>
+
+        
     </div>
 </template>
