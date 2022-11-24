a83b578ee01a10dd909e4986bcb8071819e2e803
Mosa Nameer <mosa.mn777@gmail.com>
2022-11-24 10:14:57 +0300

    add

--- pages/achievements.vue
+++ pages/achievements.vue
@@ -4,7 +4,7 @@
             <div class="w-full">
                 <NuxtLink v-for="project in projects" :key="project.id" :to="project._path">
                     <div
-                        class="grid grid-cols-3 mb-15 md:w-125 w-85 h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
+                        class="md:grid md:grid-cols-3 mb-15 md:w-125 w-72 md:h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
                         <div class="col-span-1 overflow-hidden ">
                             <img class="w-full" src="./../assets/mosanameer.jpg">
                         </div>
