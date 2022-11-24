0fc3aa361bccff0e2cf39f7bfbbe87ce5db593ca
Mosa Nameer <mosa.mn777@gmail.com>
2022-11-24 15:34:03 +0300

    add

--- pages/achievements.vue
+++ pages/achievements.vue
@@ -6,13 +6,13 @@
                     <div
                         class="md:grid md:grid-cols-3 mb-15 md:w-125 w-72 md:h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
                         <div class="col-span-1 overflow-hidden ">
-                            <img class="w-full" :src="dynamicImages[project.img]">
+                            <img class="w-full h-full" :src="dynamicImages[project.img]">
                         </div>
                         <div class="col-span-2 flex flex-col justify-between">
                             <div class="px-2">
                                 <div class="font-bold mt-1">
-                                    <h1 class="text-base md:text-lg text-dark dark:text-light">{{ project.title }} </h1> 
-                                    </div>
+                                    <h1 class="text-base md:text-lg text-dark dark:text-light">{{ project.title }} </h1>
+                                </div>
                                 <h1 class="text-gray-700 text-xs text-dark dark:text-light	">
                                     {{ project.description }}
                                 </h1>
@@ -34,7 +34,6 @@
 <script setup>
 import { filename } from "pathe/utils";
 const colorMode = useColorMode();
-
 
 const { data: projects, pending: projectPending } = await useAsyncData("projects", () =>
     queryContent("_projects").where({ _partial: true, _type: "markdown" }).find()
