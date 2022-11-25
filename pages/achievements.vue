2d0ff00ab152294b7ecd5307ac80c1945b47f1c6
Mosa Nameer <mosa.mn777@gmail.com>
2022-11-25 15:59:20 +0300

    add

--- pages/achievements.vue
+++ pages/achievements.vue
@@ -5,8 +5,11 @@
                 <NuxtLink class="no-underline" v-for="project in projects" :key="project.id" :to="project._path">
                     <div
                         class="md:grid md:grid-cols-3 mb-15 md:w-125 w-72 md:h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
-                        <div class="col-span-1 overflow-hidden ">
-                            <img class="w-full md:h-full h-72" :src="dynamicImages[project.img]">
+                        <div class="col-span-1 overflow-hidden self-center dark:bg-light">
+                            <img v-if="project.img" class="w-full md:h-full h-72" :src="dynamicImages[project.img]">
+                            <!-- <div v-if="project.img2" class="self-center flex justify-center">
+                                <img class="w-[70%] " :src="dynamicImages[project.img2]">
+                            </div> -->
                         </div>
                         <div class="col-span-2 flex flex-col justify-between">
                             <div class="px-2">
@@ -19,7 +22,7 @@
                             </div>
                             <div class="px-4 mb-2 mt-2 md:mt-0">
                                 <span v-for="tag in project.tags" :key="tag"
-                                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 mb-1">
+                                    class="inline-block bg-gray-200 rounded-full px-2 py-0.5 text-xs font-semibold text-gray-700 mr-1.5 md:mb-1">
                                     {{ tag }}
                                 </span>
                             </div>
