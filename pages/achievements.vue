0fcc80dece07fbb34967ec860867537c55c767c5
Mosa Nameer <mosa.mn777@gmail.com>
2022-11-25 11:31:37 +0300

    add

--- pages/achievements.vue
+++ pages/achievements.vue
@@ -2,7 +2,7 @@
     <div class="max-w-4xl grid grid-cols-1 gap-8 justify-items-center  py-10 m-auto sm:px-8 text-dark dark:text-light">
         <div>
             <div class="w-full">
-                <NuxtLink v-for="project in projects" :key="project.id" :to="project._path">
+                <NuxtLink class="no-underline" v-for="project in projects" :key="project.id" :to="project._path">
                     <div
                         class="md:grid md:grid-cols-3 mb-15 md:w-125 w-72 md:h-40 rounded overflow-hidden shadow-lg bg-light dark:bg-dark mx-2 drop-shadow-[2px_2px_0px_#4F009D] border-2 border-dark dark:border-light transition ease-in-out hover:-translate-y-1 hover:drop-shadow-[4px_4px_0px_#4F009D] duration-200">
                         <div class="col-span-1 overflow-hidden ">
@@ -11,11 +11,11 @@
                         <div class="col-span-2 flex flex-col justify-between">
                             <div class="px-2">
                                 <div class="font-bold mt-1">
-                                    <h1 class="text-base md:text-lg text-dark dark:text-light"> {{ project.title }} </h1>
+                                    <p class="text-base md:text-lg text-dark dark:text-light"> {{ project.title }} </p>
                                 </div>
-                                <h1 class="text-gray-700 text-xs text-dark dark:text-light">
+                                <p class="text-gray-700 text-xs text-dark dark:text-light">
                                     {{ project.description }}
-                                </h1>
+                                </p>
                             </div>
                             <div class="px-4 mb-2 mt-2 md:mt-0">
                                 <span v-for="tag in project.tags" :key="tag"
@@ -45,8 +45,3 @@ const dynamicImages = Object.fromEntries(
 );
 </script>
 
-<style>
-h1 {
-    text-decoration: none;
-}
-</style>
