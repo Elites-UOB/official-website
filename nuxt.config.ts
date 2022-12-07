2e93a5c5debe0310714608a33e047e85eb4ee44d
Mosa Nameer <mosa.mn777@gmail.com>
2022-12-07 15:56:32 +0300

    add

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,28 +4,43 @@ export default defineNuxtConfig({
     inlineSSRStyles: false,
   },
   modules: [
-    '@vueuse/nuxt',
-    '@nuxt/content',
+    "@vueuse/nuxt",
+    "@nuxt/content",
     // '@nuxtjs/tailwindcss',
-    '@nuxtjs/color-mode',
-    'nuxt-icon',
-    '@unocss/nuxt',
-    '@nuxtjs/sitemap',
+    "@nuxtjs/color-mode",
+    "nuxt-icon",
+    "@unocss/nuxt",
+    "@nuxtjs/sitemap",
   ],
-  css: [
-    '@/assets/scss/main.scss',
-  ],
+  sitemap: {
+    hostname: 'https://csitelites.tech',
+    lastmod: '2017-06-30',
+    sitemaps: [
+      {
+        path: '/sitemap-foo.xml',
+        routes: ['foo/1', 'foo/2'],
+        gzip: true
+      },
+      {
+        path: '/folder/sitemap-bar.xml',
+        routes: ['bar/1', 'bar/2'],
+        exclude: ['/**']
+      }
+    ],
+  },
+  
+  css: ["@/assets/scss/main.scss"],
   // https://color-mode.nuxtjs.org
   colorMode: {
-    classSuffix: ''
+    classSuffix: "",
   },
   // https://content.nuxtjs.org
   content: {
     documentDriven: true,
     highlight: {
       // See the available themes on https://github.com/shikijs/shiki/blob/main/docs/themes.md#all-theme
-      theme: 'vitesse-dark'
-    }
+      theme: "vitesse-dark",
+    },
   },
   unocss: {
     // presets
@@ -41,41 +56,49 @@ export default defineNuxtConfig({
       fonts: {
         tajawal: [
           {
-            name: 'Tajawal',
-            weights: ['200','300','400','500','600','700','800'],
+            name: "Tajawal",
+            weights: ["200", "300", "400", "500", "600", "700", "800"],
           },
           {
-            name: 'sans-serif',
-            provider: 'none',
+            name: "sans-serif",
+            provider: "none",
           },
         ],
       },
     },
 
     theme: {
-      backgroundImage: { 
-        'gradient-radial': 'radial-gradient(ellipse at center, var(--tw-gradient-stops))', 
-        'gradient-radial-t': 'radial-gradient(ellipse at top, var(--tw-gradient-stops))', 
-        'gradient-radial-b': 'radial-gradient(ellipse at bottom, var(--tw-gradient-stops))', 
-        'gradient-radial-l': 'radial-gradient(ellipse at left, var(--tw-gradient-stops))', 
-        'gradient-radial-r': 'radial-gradient(ellipse at right, var(--tw-gradient-stops))', 
-        'gradient-radial-tl': 'radial-gradient(ellipse at top left, var(--tw-gradient-stops))', 
-        'gradient-radial-tr': 'radial-gradient(ellipse at top right, var(--tw-gradient-stops))', 
-        'gradient-radial-bl': 'radial-gradient(ellipse at bottom left, var(--tw-gradient-stops))', 
-        'gradient-radial-br': 'radial-gradient(ellipse at bottom right, var(--tw-gradient-stops))', 
-      }, 
+      backgroundImage: {
+        "gradient-radial":
+          "radial-gradient(ellipse at center, var(--tw-gradient-stops))",
+        "gradient-radial-t":
+          "radial-gradient(ellipse at top, var(--tw-gradient-stops))",
+        "gradient-radial-b":
+          "radial-gradient(ellipse at bottom, var(--tw-gradient-stops))",
+        "gradient-radial-l":
+          "radial-gradient(ellipse at left, var(--tw-gradient-stops))",
+        "gradient-radial-r":
+          "radial-gradient(ellipse at right, var(--tw-gradient-stops))",
+        "gradient-radial-tl":
+          "radial-gradient(ellipse at top left, var(--tw-gradient-stops))",
+        "gradient-radial-tr":
+          "radial-gradient(ellipse at top right, var(--tw-gradient-stops))",
+        "gradient-radial-bl":
+          "radial-gradient(ellipse at bottom left, var(--tw-gradient-stops))",
+        "gradient-radial-br":
+          "radial-gradient(ellipse at bottom right, var(--tw-gradient-stops))",
+      },
       colors: {
-        'dark': "#0F0F11",
-        'light': "#F3F3F3",
-        'mid': "#B7B7B7",
+        dark: "#0F0F11",
+        light: "#F3F3F3",
+        mid: "#B7B7B7",
       },
     },
   },
 
-
   nitro: {
     prerender: {
-      routes: ['/sitemap.xml']
-    }
-  }
-})
+      routes: ["/sitemap.xml"],
+    },
+  },
+});
