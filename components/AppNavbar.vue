a58a19c4260b0150c802bf37ec71522501601128
Mosa Nameer <mosa.mn777@gmail.com>
2022-12-10 17:27:39 +0300

    add

--- components/AppNavbar.vue
+++ components/AppNavbar.vue
@@ -4,9 +4,9 @@ const appConfig = useAppConfig()
 </script>
 
 <template>
-  <div class="flex justify-between max-w-2xl px-4 py-4 mx-auto sm:px-8">
+  <div class="flex justify-between max-w-2xl px-4 py-4 mx-auto flex-col md:flex-row sm:px-8">
     <!-- Navigation -->
-    <div class="text-mid flex">
+    <div class="flex text-mid flex">
       <NuxtLink
         v-for="link of navigation"
         :key="link._path"
@@ -19,7 +19,7 @@ const appConfig = useAppConfig()
       </NuxtLink>
     </div>
     <!-- Social icons & Color Mode -->
-    <div class="flex transition text-primary-500">
+    <div class="flex justify-end transition text-primary-500 mt-2 md:mt-0">
       <a v-if="appConfig.socials?.telegram" :href="`https://${appConfig.socials?.telegram}.t.me`" title="Telegram" class="mx-2 text-mid hover:text-dark dark:hover:text-light"><Icon name="fa-brands:telegram" class="w-5 h-5" /></a>
       <a v-if="appConfig.socials?.organisation" :href="`https://github.com/Elites-UOB`" title="organisation" class="mx-2 text-mid hover:text-dark dark:hover:text-light"><Icon name="fa-brands:github" class="w-5 h-5" /></a>
       <ColorModeSwitch class="mx-2 text-mid hover:text-dark dark:hover:text-light" />
